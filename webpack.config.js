756cd6d4750dc70dca3e55e39baed07dbe9f176b
kmsheng <kmsh3ng@gmail.com>
2019-12-07 00:21:12 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode,
-  entry: './src/index.js',
+  entry: './src/js/index.js',
   module: {
     rules: [
       {
