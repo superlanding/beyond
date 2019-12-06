eab606c132ac6810204c2fa14fc91cdb7bd684b8
kmsheng <kmsh3ng@gmail.com>
2019-12-06 23:08:24 +0800

    Revert "add _site folder"
    This reverts commit bc183921b02fd9921eb43da1f5b3272e1c211ea7.

--- _site/assets/js/default.js
+++
@@ -1,9 +0,0 @@
-const { Codebox, Tabbox } = window.beyond
-
-document.querySelectorAll('[data-codebox]').forEach(dom => {
-  const codebox = new Codebox(dom)
-})
-
-document.querySelectorAll('[data-tabbox]').forEach(dom => {
-  const tabbox = new Tabbox(dom)
-})
