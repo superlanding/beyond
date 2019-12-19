68dfd53096eadb866cb1e9ba5653aac621ed6ed3
kmsheng <kmsh3ng@gmail.com>
2019-12-19 15:46:32 +0800

    re-style

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,28 +1,32 @@
 const { Codebox, Dropdown, Tabbox, Modal } = window.beyond
 
-document.querySelectorAll('[data-codebox]').forEach(dom => {
-  const codebox = new Codebox(dom)
-})
-
-document.querySelectorAll('[data-tabbox]').forEach(dom => {
-  const tabbox = new Tabbox(dom, {
-    onChange(data) {
-      console.log('onChange', data)
-    }
+document.querySelectorAll('[data-codebox]')
+  .forEach(dom => {
+    const codebox = new Codebox(dom)
   })
-})
 
-document.querySelectorAll('[data-dropdown]').forEach(dom => {
-  const dropdown = new Dropdown(dom)
-})
-
-document.querySelectorAll('[data-modal-opener]').forEach(dom => {
-  const modal = new Modal(dom, {
-    onConfirm() {
-      console.log('confirmed')
-    },
-    onCancel(type) {
-      console.log('cancelled', type)
-    }
+document.querySelectorAll('[data-tabbox]')
+  .forEach(dom => {
+    const tabbox = new Tabbox(dom, {
+      onChange(data) {
+        console.log('onChange', data)
+      }
+    })
   })
-})
+
+document.querySelectorAll('[data-dropdown]')
+  .forEach(dom => {
+    const dropdown = new Dropdown(dom)
+  })
+
+document.querySelectorAll('[data-modal-opener]')
+  .forEach(dom => {
+    const modal = new Modal(dom, {
+      onConfirm() {
+        console.log('confirmed')
+      },
+      onCancel(type) {
+        console.log('cancelled', type)
+      }
+    })
+  })
