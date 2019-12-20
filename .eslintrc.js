dc114cb47599699ab7b4b9c7bdbe15fbaf9f2b00
kmsheng <kmsh3ng@gmail.com>
2019-12-20 13:48:09 +0800

    add rules to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,16 @@ module.exports = {
   extends: 'google',
   parserOptions: {
     sourceType: 'module'
+  },
+  rules: {
+    "require-jsdoc": "off",
+    "padded-blocks": "off",
+    "semi": "off",
+    "max-len": ["error", { "code": 90, "comments": 90 }],
+    "arrow-parens": "off",
+    "brace-style": ["error", "stroustrup"],
+    "comma-dangle": "off",
+    "object-curly-spacing": ["error", "always"],
+    "space-before-function-paren": "off"
   }
 }
