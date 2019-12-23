4b2d8f0d1c4d3011e571517f835a1eb3e77550d5
kmsheng <kmsh3ng@gmail.com>
2019-12-23 20:02:16 +0800

    fix typo: date22 -> date2

--- src/js/helpers/dateLt.js
+++ src/js/helpers/dateLt.js
@@ -1,5 +1,5 @@
 import compareAsc from 'date-fns/compareAsc'
 
-export default function dateLt(date1, date22) {
+export default function dateLt(date1, date2) {
   return compareAsc(date1, date2) === -1
 }
