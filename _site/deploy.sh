c519d78d7130bb5ce380d21ec71fd4b0136d8749
kmsheng <kmsh3ng@gmail.com>
2019-12-25 18:23:26 +0800

    update _site/

---
+++ _site/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git co master
+yarn build
+git add dist
+git commit -m "update dist/"
+
+git co github
+git merge master --no-ff --no-edit
+
+git add -f _site
+git commit -m "update _site/"
+git subtree split --prefix _site -b gh-pages
+
+git push --all origin
