0ffe82b6a966e2c0f130096c03342a8d661151eb
kmsheng <kmsh3ng@gmail.com>
2019-12-25 18:19:53 +0800

    update _site/

--- _site/pre-deploy.sh
+++ _site/pre-deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+git co master
+yarn build
+git add dist
+git commit -m "update dist/"
+
 git co github
 git merge master --no-ff
-yarn build
-git add -f _site dist
-git commit -m "update _site/ and dist/"
+
+git add -f _site
+git commit -m "update _site/"
 git subtree split --prefix _site -b gh-pages
