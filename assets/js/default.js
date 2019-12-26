d1bccdbc09fcd4bbe1272efe8d3badf95b5fd9c0
kmsheng <kmsh3ng@gmail.com>
2019-12-26 13:15:42 +0800

    move tooltip demo js to page tooltips

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,13 +1,8 @@
 (function() {
-  const { Codebox, Modal, Tooltip } = window.beyond
+  const { Codebox } = window.beyond
 
   document.querySelectorAll('[data-codebox]')
     .forEach(dom => {
       const codebox = new Codebox(dom)
-    })
-
-  document.querySelectorAll('[data-tooltip]')
-    .forEach(dom => {
-      const tooltip = new Tooltip(dom)
     })
 })()
