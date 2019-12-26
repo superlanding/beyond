1b434ec21e594cd37057defbf4ccf39b258c35d6
kmsheng <kmsh3ng@gmail.com>
2019-12-26 13:52:44 +0800

    add Autocomplete model

---
+++ _includes/demos/autocompletes.js
@@ -0,0 +1,4 @@
+const { Autocomplete } = window.beyond
+
+const dom = document.querySelector('[data-autocomplete]')
+const autocomplete = new Autocomplete(dom)
---
+++ src/js/components/Autocomplete.js
@@ -0,0 +1,18 @@
+export default class Autocomplete {
+
+  constructor(dom, options = {}) {
+    this.dom = dom
+    this.options = options
+    this.init()
+  }
+
+  init() {
+    this.addEvents()
+  }
+
+  addEvents() {
+  }
+
+  destroy() {
+  }
+}
