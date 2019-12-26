d091c180a0c51ec3c50e919e963481a63ffebad5
kmsheng <kmsh3ng@gmail.com>
2019-12-26 13:14:11 +0800

    move modal demo js to page modals

--- assets/js/default.js
+++ assets/js/default.js
@@ -6,18 +6,6 @@
       const codebox = new Codebox(dom)
     })
 
-  document.querySelectorAll('[data-modal-opener]')
-    .forEach(dom => {
-      const modal = new Modal(dom, {
-        confirm() {
-          console.log('confirmed')
-        },
-        cancel(type) {
-          console.log('cancelled', type)
-        }
-      })
-    })
-
   document.querySelectorAll('[data-tooltip]')
     .forEach(dom => {
       const tooltip = new Tooltip(dom)
