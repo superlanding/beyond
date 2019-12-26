9bbf1fa1dbee5817858e07b33a6205d62af9355b
kmsheng <kmsh3ng@gmail.com>
2019-12-26 13:10:33 +0800

    move tabbox demo js to components/buttons.html

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,32 +1,25 @@
-const { Codebox, Tabbox, Modal, Tooltip } = window.beyond
+(function() {
+  const { Codebox, Modal, Tooltip } = window.beyond
 
-document.querySelectorAll('[data-codebox]')
-  .forEach(dom => {
-    const codebox = new Codebox(dom)
-  })
-
-document.querySelectorAll('[data-tabbox]')
-  .forEach(dom => {
-    const tabbox = new Tabbox(dom, {
-      change(data) {
-        console.log('change', data)
-      }
+  document.querySelectorAll('[data-codebox]')
+    .forEach(dom => {
+      const codebox = new Codebox(dom)
     })
-  })
 
-document.querySelectorAll('[data-modal-opener]')
-  .forEach(dom => {
-    const modal = new Modal(dom, {
-      confirm() {
-        console.log('confirmed')
-      },
-      cancel(type) {
-        console.log('cancelled', type)
-      }
+  document.querySelectorAll('[data-modal-opener]')
+    .forEach(dom => {
+      const modal = new Modal(dom, {
+        confirm() {
+          console.log('confirmed')
+        },
+        cancel(type) {
+          console.log('cancelled', type)
+        }
+      })
     })
-  })
 
-document.querySelectorAll('[data-tooltip]')
-  .forEach(dom => {
-    const tooltip = new Tooltip(dom)
-  })
+  document.querySelectorAll('[data-tooltip]')
+    .forEach(dom => {
+      const tooltip = new Tooltip(dom)
+    })
+})()
