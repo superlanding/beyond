53920e77a5ccea15dab2f0c23009ee0d50e4a4db
kmsheng <kmsh3ng@gmail.com>
2019-12-31 14:13:42 +0800

    update _site/ and dist/

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,5 +1,5 @@
 (function() {
-  const { Codebox, Sidebar } = window.beyond
+  const { Codebox, Sidebar } = window.beyond.default
 
   document.querySelectorAll('[data-codebox]')
     .forEach(dom => {
