ad81d55faf74a08a1f2de9aa656a97c499c44a7c
kmsheng <kmsh3ng@gmail.com>
2020-01-14 16:06:03 +0800

    update _site/ and dist/

---
+++ webpack/lib.config.js
@@ -0,0 +1,78 @@
+const path = require('path')
+const mode = (process.env.NODE_ENV === 'production') ? 'production' : 'development'
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode,
+  entry: './src/js/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-url-loader'
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            }
+          }
+        ],
+        exclude: /src\/sass\/img/
+      },
+      {
+        test: /\.css$/,
+        use: ['postcss-loader']
+      },
+      {
+       test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: (process.env.NODE_ENV === 'development')
+            }
+          },
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 1 }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [require('autoprefixer')]
+            }
+          },
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  output: {
+    library: 'beyond',
+    libraryTarget: 'umd',
+    filename: 'beyond.js',
+    path: path.resolve(__dirname, '../dist')
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'beyond.css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false
+    })
+  ],
+  devServer: {
+    headers: {
+      "Access-Control-Allow-Origin": "http://localhost:4000"
+    }
+  }
+}
