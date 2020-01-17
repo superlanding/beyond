3ee1ca4e40618436ef381b039b788f342ddf014b
kmsheng <kmsh3ng@gmail.com>
2020-01-17 10:47:08 +0800

    add gem related files

---
+++ lib/beyond/engine.rb
@@ -0,0 +1,17 @@
+module Beyond
+  module Rails
+    class Engine < ::Rails::Engine
+      initializer 'beyond.assets.precompile' do |app|
+        %w(stylesheets javascripts fonts images).each do |sub|
+          app.config.assets.paths << root.join('assets', sub).to_s
+        end
+
+        # sprockets-rails 3 tracks down the calls to `font_path` and `image_path`
+        # and automatically precompiles the referenced assets.
+        unless Sprockets::Rails::VERSION.split('.', 2)[0].to_i >= 3
+          app.config.assets.precompile << %r(icomoon\.(?:eot|svg|ttf|woff2?)$)
+        end
+      end
+    end
+  end
+end
