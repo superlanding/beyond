a3cd911b56a231169e07859cdf93bc408b53d5ea
kmsheng <kmsh3ng@gmail.com>
2020-01-20 16:50:50 +0800

    add two more styles of navs

--- _includes/demos/navbars.js
+++ _includes/demos/navbars.js
@@ -2,7 +2,5 @@
   const { Navbar } = window.beyond
   const navbarDoms = Array.from(document.querySelectorAll('[data-navbar]'))
 
-  navbarDoms.forEach(navbarDom => {
-    const navbar = new Navbar(navbarDom)
-  })
+  navbarDoms.forEach(navbarDom => new Navbar(navbarDom))
 })()
--- src/js/components/Navbar.js
+++ src/js/components/Navbar.js
@@ -19,12 +19,14 @@ export default class Navbar {
   hideMenu() {
     this.btn.classList.remove('js-active')
     this.menu.classList.remove('js-collapse')
+    this.dom.classList.remove('js-collapse')
     this.isMenuVisible = false
   }
 
   showMenu() {
     this.btn.classList.add('js-active')
     this.menu.classList.add('js-collapse')
+    this.dom.classList.add('js-collapse')
     this.isMenuVisible = true
   }
 
@@ -34,5 +36,12 @@ export default class Navbar {
 
   addEvents() {
     this.addEvent(this.btn, 'click', () => this.toggleMenu())
+    this.addEvent(document, 'click', ({ target }) => {
+      const { dom } = this
+      if ((dom === target) || dom.contains(event.target)) {
+        return
+      }
+      this.hideMenu()
+    })
   }
 }
