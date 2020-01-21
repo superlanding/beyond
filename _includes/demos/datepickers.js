3df86458bf6bd2de7ecd84ca48b04cf0625ce278
kmsheng <kmsh3ng@gmail.com>
2020-01-21 20:40:37 +0800

    rename data-datepicker -> data-date-time-ranger

--- _includes/demos/datepickers.js
+++ _includes/demos/datepickers.js
@@ -5,7 +5,7 @@ const { DateTimeRanger } = window.beyond
 
 intlReady()
   .then(() => {
-    document.querySelectorAll('[data-datepicker]')
+    document.querySelectorAll('[data-date-time-ranger]')
       .forEach(dom => new DateTimeRanger(dom, {
         startDate: zonedTimeToUtc('2018-09-01 18:01:36.386', 'Asia/Taipei'),
         endDate: zonedTimeToUtc('2018-09-03 18:01:36.386', 'Asia/Taipei')
