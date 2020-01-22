678e0e68e54339f1f7e81fa466c642233e0e9cad
kmsheng <kmsh3ng@gmail.com>
2020-01-22 09:30:20 +0800

    prevent ios keyboard in phone

--- src/js/components/DateInput.js
+++ src/js/components/DateInput.js
@@ -1,6 +1,7 @@
 import { format } from 'date-fns-tz'
 import { DEFAULT_TIMEZONE } from '../consts'
 import supportDom from '../helpers/supportDom'
+import isTouchDevice from '../helpers/isTouchDevice'
 
 @supportDom
 export default class DateInput {
@@ -21,12 +22,19 @@ export default class DateInput {
   }
 
   init() {
+    this.initInput()
+    this.addEvents()
+  }
+
+  initInput() {
     const { dom } = this
     dom.value = this.format(this.date)
     if (! dom.hasAttribute('placeholder')) {
       dom.setAttribute('placeholder', this.datePattern.toUpperCase())
     }
-    this.addEvents()
+    if (isTouchDevice()) {
+      dom.setAttribute('readonly', 'readonly')
+    }
   }
 
   format(date) {
