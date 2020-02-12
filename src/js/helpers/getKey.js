284f6a813f7e8778371f943b7ee6f085e1bcc65f
kmsheng <kmsh3ng@gmail.com>
2020-02-12 18:10:33 +0800

    add helper getKey

---
+++ src/js/helpers/getKey.js
@@ -0,0 +1,14 @@
+const keyMap = {
+  13: 'enter',
+  17: 'ctrl',
+  27: 'esc',
+  38: 'up',
+  40: 'down',
+  70: 'f',
+  91: 'left-meta',
+  93: 'right-meta'
+}
+
+export default function getKey(event) {
+  return keyMap[event.keyCode] || ''
+}
