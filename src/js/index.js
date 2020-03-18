df5c0f439b87e8e606729d7ed1ea6101e9041763
kmsheng <kmsh3ng@gmail.com>
2020-03-18 12:03:17 +0800

    re-style

--- src/js/index.js
+++ src/js/index.js
@@ -5,18 +5,18 @@ import './polyfills/classList'
 import './polyfills/nodeRemove'
 import './polyfills/elementDataset'
 import Autocomplete from './components/Autocomplete'
+import Checkbox from './components/Checkbox'
 import DateTimeRanger from './components/DateTimeRanger'
 import Datepicker from './components/Datepicker'
 import Dropdown from './components/Dropdown'
 import Modal from './components/Modal'
+import Navbar from './components/Navbar'
+import Radio from './components/Radio'
+import SearchDropdown from './components/SearchDropdown'
+import Sidebar from './components/Sidebar'
 import Tabbox from './components/Tabbox'
 import Toast from './components/Toast'
 import Tooltip from './components/Tooltip'
-import Sidebar from './components/Sidebar'
-import Navbar from './components/Navbar'
-import SearchDropdown from './components/SearchDropdown'
-import Radio from './components/Radio'
-import Checkbox from './components/Checkbox'
 
 export {
   Autocomplete,
@@ -25,11 +25,11 @@ export {
   Datepicker,
   Dropdown,
   Modal,
+  Navbar,
+  Radio,
+  SearchDropdown,
   Sidebar,
   Tabbox,
   Toast,
-  Tooltip,
-  Navbar,
-  Radio,
-  SearchDropdown
+  Tooltip
 }
