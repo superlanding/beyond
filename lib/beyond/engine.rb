adb8074998d6c329401f31c8f0f52a9994297513
kmsheng <kmsh3ng@gmail.com>
2020-03-19 15:36:42 +0800

    update engine.rb

--- lib/beyond/engine.rb
+++ lib/beyond/engine.rb
@@ -2,8 +2,13 @@ module Beyond
   module Rails
     class Engine < ::Rails::Engine
       initializer 'beyond.assets.precompile' do |app|
-        %w(sass js font img).each do |sub|
+
+        %w(sass).each do |sub|
           app.config.assets.paths << root.join('src', sub).to_s
+        end
+
+        %w(js font img).each do |sub|
+          app.config.assets.paths << root.join('dist', sub).to_s
         end
 
         # sprockets-rails 3 tracks down the calls to `font_path` and `image_path`
