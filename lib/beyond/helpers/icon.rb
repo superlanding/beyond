d6850a097a0814012363d68b8b85655330e2a851
kmsheng <kmsh3ng@gmail.com>
2020-04-06 17:18:30 +0800

    update _site/ and dist/

--- lib/beyond/helpers/icon.rb
+++ lib/beyond/helpers/icon.rb
@@ -1,8 +1,23 @@
 module Beyond
   module Helpers
     module Icon
-      def b_icon(i, text=nil)
-        tag = content_tag(:span, content_tag(:i, "", class: "icon-#{i}"), class: "icon")
+      def b_icon(i, *args)
+        text = nil
+        options = {}
+
+        if args.length == 1
+          if args[0].is_a?(String)
+            text = args.shift
+          else
+            options = args.shift
+          end
+        elsif args.length == 2
+          text = args.shift
+          options = args.shift
+        end
+
+        klass = options.delete(:class) { nil }
+        tag = content_tag(:i, "", options.merge(class: "icon icon-#{i} #{klass}"))
         text ? [tag, text].join(" ").html_safe : tag
       end
     end
