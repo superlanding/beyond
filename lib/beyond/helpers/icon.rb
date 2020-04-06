80b4640d588029a5535d9deafae19567d9cd1132
eddie <eddie.li.624@gmail.com>
2020-04-06 16:28:58 +0800

    fix icon helper

--- lib/beyond/helpers/icon.rb
+++ lib/beyond/helpers/icon.rb
@@ -1,8 +1,20 @@
 module Beyond
   module Helpers
     module Icon
-      def b_icon(i, text=nil)
-        tag = content_tag(:span, content_tag(:i, "", class: "icon-#{i}"), class: "icon")
+      def b_icon(i, *args)
+        if args.is_a?(Hash)
+          text = nil
+          options = args
+        elsif args.is_a?(Array)
+          text = args.shift
+          options = args.shift || {}
+        else
+          text = nil
+          options = {}
+        end
+
+        klass = options.delete(:class) { nil }
+        tag = content_tag(:i, "", options.merge(class: "icon icon-#{i} #{klass}"))
         text ? [tag, text].join(" ").html_safe : tag
       end
     end
