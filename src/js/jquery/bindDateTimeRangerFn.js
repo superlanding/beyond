3bf0eeb1b95b101ec78e244ca81433b5ee3d27ec
kmsheng <kmsh3ng@gmail.com>
2020-04-09 19:20:36 +0800

    remove debug code

--- src/js/jquery/bindDateTimeRangerFn.js
+++ src/js/jquery/bindDateTimeRangerFn.js
@@ -5,7 +5,6 @@ export default function bindDateTimeRangerFn($) {
   $.fn.dateTimeRanger = function(settings) {
 
     const options = $.extend({}, $.fn.dateTimeRanger.defaults, settings)
-    console.log(options)
     const dateTimeRangers = this.map((i, dom) => new DateTimeRanger(dom, options))
 
     this.destroy = () => dateTimeRangers.each((i, d) => d.destroy())
