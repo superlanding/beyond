b61081d937091c2dfdbdf523323ce8227f6d8a79
kmsheng <kmsh3ng@gmail.com>
2020-04-09 18:05:07 +0800

    add alert.destroy method

--- src/js/jquery/bindAlertFn.js
+++ src/js/jquery/bindAlertFn.js
@@ -2,7 +2,8 @@ import Alert from '../components/Alert'
 
 export default function bindAlertFn($) {
   $.fn.alert = function() {
-    this.each((i, dom) => new Alert(dom))
+    const alerts = this.map((i, dom) => new Alert(dom))
+    this.destroy = () => alerts.each((i, a) => a.destroy())
     return this
   }
 }
