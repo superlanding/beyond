e863f2b7013a464dd3d6da0d3015900f06ecc32b
kmsheng <kmsh3ng@gmail.com>
2020-04-09 17:07:22 +0800

    add method bindAutocompleteFn

---
+++ src/js/jquery/bindAutocompleteFn.js
@@ -0,0 +1,8 @@
+import Autocomplete from '../components/Autocomplete'
+
+export default function bindAutocompleteFn($) {
+  $.fn.autocomplete = function(options) {
+    this.each((i, dom) => new Autocomplete(dom, options))
+    return this
+  }
+}
