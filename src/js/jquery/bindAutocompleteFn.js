29ec7a8f719ee7046e790ca9f82d822b50f7d330
kmsheng <kmsh3ng@gmail.com>
2020-04-09 18:17:19 +0800

    add destroy method to $.fn.autocomplete

--- src/js/jquery/bindAutocompleteFn.js
+++ src/js/jquery/bindAutocompleteFn.js
@@ -3,7 +3,11 @@ import Autocomplete from '../components/Autocomplete'
 export default function bindAutocompleteFn($) {
 
   $.fn.autocomplete = function(options) {
-    this.each((i, dom) => new Autocomplete(dom, options))
+
+    const autocompletes = this.map((i, dom) => new Autocomplete(dom, options))
+
+    this.destroy = () => autocompletes.each((i, a) => a.destroy())
+
     return this
   }
 }
