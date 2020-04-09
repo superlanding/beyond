a5b243cff2d190b427e9d4a354cf7b1632cdf802
kmsheng <kmsh3ng@gmail.com>
2020-04-09 18:18:21 +0800

    add destroy method to $.fn.dropdown

--- src/js/jquery/bindDropdownFn.js
+++ src/js/jquery/bindDropdownFn.js
@@ -2,7 +2,11 @@ import Dropdown from '../components/Dropdown'
 
 export default function bindDropdownFn($) {
   $.fn.dropdown = function() {
-    this.each((i, dom) => new Dropdown(dom))
+
+    const dropdowns = this.map((i, dom) => new Dropdown(dom))
+
+    this.destroy = () => dropdowns.each((i, d) => d.destroy())
+
     return this
   }
 }
