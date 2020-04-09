1e87a029977141a0c7cf1b181997b26deac72715
kmsheng <kmsh3ng@gmail.com>
2020-04-09 18:34:11 +0800

    add destroy method $.fn.sidebar

--- src/js/jquery/bindSidebarFn.js
+++ src/js/jquery/bindSidebarFn.js
@@ -3,7 +3,11 @@ import Sidebar from '../components/Sidebar'
 export default function bindSidebarFn($) {
 
   $.fn.sidebar = function() {
-    this.each((i, dom) => new Sidebar(dom))
+
+    const sidebars = this.map((i, dom) => new Sidebar(dom))
+
+    this.destroy = () => sidebars.each((i, s) => s.destroy())
+
     return this
   }
 }
