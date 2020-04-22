808f088ae1504b050e8973702b55fee4fa561c12
eddie <eddie.li.624@gmail.com>
2020-04-22 16:59:23 +0800

    補上 TODO

--- release.sh
+++ release.sh
@@ -19,6 +19,17 @@ NEXT_VERSION=$(npm version patch | sed -e 's/v/''/g')
 # gem push beyond-rails-$NEXT_VERSION.gem
 # rm beyond-rails-$NEXT_VERSION.gem
 
+# 說明：
+# Ruby gem 版本升級，必須做以下步驟：
+# 1. Beyond::Version (lib/beyond/version.rb) 更新版號 (不能有v開頭)
+# 2. Git Commit
+# 3. gem build beyond.gemspec (壓縮成 gem)
+# 4. gem push beyond-rails-$NEXT_VERSION.gem
+# 5. 刪除 beyond-rails-$NEXT_VERSION.gem
+# 
+# Gem 網站：https://rubygems.org/gems/beyond-rails
+# 關於 Gem publish 的 permission 已經增加 (kmsh3ng@gmail.com) 請去註冊 rubygems.org 網站
+
 git push --tags
 bash -l -c "npm publish"
 
