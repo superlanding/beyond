2592156b9ff45c90d92810bdbd77fb155dc85c98
eddie <eddie.li.624@gmail.com>
2020-04-22 16:54:48 +0800

    update release

--- release.sh
+++ release.sh
@@ -11,13 +11,13 @@ fi
 yarn deploy
 # NPM release
 NEXT_VERSION=$(npm version patch | sed -e 's/v/''/g')
-# Ruby Gem release
-ruby -e "version_file = File.read('lib/beyond/version.rb'); content = version_file.gsub(/VERSION\ =\ '([0-9\.]+)'.freeze/, \"VERSION = '${NEXT_VERSION}'.freeze\"); File.write('lib/beyond/version.rb', content)"
-git add lib/beyond/version.rb
-git commit -m "Rybygem bump version: ${NEXT_VERSION}"
-gem build beyond.gemspec
-gem push beyond-rails-$NEXT_VERSION.gem
-rm beyond-rails-$NEXT_VERSION.gem
+# Ruby Gem release (TODO: 幫忙 FIX)
+# ruby -e "version_file = File.read('lib/beyond/version.rb'); content = version_file.gsub(/VERSION\ =\ '([0-9\.]+)'.freeze/, \"VERSION = '${NEXT_VERSION}'.freeze\"); File.write('lib/beyond/version.rb', content)"
+# git add lib/beyond/version.rb
+# git commit -m "Rybygem bump version: ${NEXT_VERSION}"
+# gem build beyond.gemspec
+# gem push beyond-rails-$NEXT_VERSION.gem
+# rm beyond-rails-$NEXT_VERSION.gem
 
 git push --tags
 bash -l -c "npm publish"
