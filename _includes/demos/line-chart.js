c0c2ed6458f9080feba9db06d9a689815fa848e1
kmsheng <kmsh3ng@gmail.com>
2020-09-03 01:30:32 +0800

    update demos/line-chart.js

--- _includes/demos/line-chart.js
+++ _includes/demos/line-chart.js
@@ -4,18 +4,33 @@ export default function bindCharts() {
 
   const { Chart } = window.beyond
   const dom = document.getElementById('line-chart')
+  const padZero = v => v.toString().padStart(2, '0')
   const xLabel = v => {
     const d = new Date(v)
-    return d.getHours() + ':' + d.getMinutes()
+    return padZero(d.getHours()) + ':' + padZero(d.getMinutes())
   }
   const yLabel = v => {
     return (v / 10000).toFixed(1) + '萬'
   }
-  const c = new Chart(dom, { xLabel, yLabel })
+
+  const fiveMins = 5 * 60 * 1000
+  const c = new Chart(dom, {
+    xLabel,
+    yLabel,
+    width: 400,
+    xStep: fiveMins,
+    yStep: 2 * 10000
+  })
 
   const now = +new Date()
-  const fiveMins = 5 * 60 * 1000
-  const points = range(1, 20 + 1)
+  const points1 = range(1, 20 + 1)
+    .map(i => {
+      return {
+        x: now + (i * fiveMins),
+        y: i * 5000
+      }
+    })
+  const points2 = range(1, 20 + 1)
     .map(i => {
       return {
         x: now + (i * fiveMins),
@@ -23,7 +38,7 @@ export default function bindCharts() {
       }
     })
 
-  c.setPoints([points])
+  c.setPoints([points1, points2])
 
   return function unbindCharts() {
   }
