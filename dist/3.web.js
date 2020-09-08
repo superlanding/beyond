b1e3d3cedf4fdc29c5e8cd323f5de3189e9ec51f
kmsheng <kmsh3ng@gmail.com>
2020-09-08 16:20:30 +0800

    yarn build

--- dist/3.web.js
+++ dist/3.web.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{5:function(n,w){}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{9:function(n,w){}}]);
 //# sourceMappingURL=3.web.js.map
