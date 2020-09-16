30747c9b705f62c390be8cd163187a00bbf47855
kmsheng <kmsh3ng@gmail.com>
2020-09-16 13:53:46 +0800

    add static date menu demo

--- _includes/demos/datepickers.js
+++ _includes/demos/datepickers.js
@@ -1,11 +1,13 @@
 import intlReady from '../../assets/js/intlReady'
+import set from 'date-fns/set'
 
 export default function bindDatepickers() {
-  const { Datepicker, Timepicker } = window.beyond
+  const { DateMenu, Datepicker, Timepicker } = window.beyond
   const dateToTimestamp = date => parseInt(+date / 1000, 10)
 
   let datepickers = []
   let timepickers = []
+  let dateMenus = []
   let unbound = false
 
   intlReady()
@@ -19,11 +21,26 @@ export default function bindDatepickers() {
 
       timepickers = Array.from(document.querySelectorAll('[data-timepicker]'))
         .map(dom => new Timepicker(dom, timestamp))
+
+      const date = new Date()
+
+      dateMenus = Array.from(document.querySelectorAll('[data-date-menu]'))
+        .map(dom => new DateMenu({ date, options: { dom, isStatic: true } }))
+        .forEach(d => {
+          d.on('td-click', (event, res) => {
+            const { year, month, date } = res
+            const newDate = set(new Date(), { year, month, date })
+            d.setDate({ startDate: newDate })
+          })
+          d.setDate({ date, startDate: date })
+          d.show()
+        })
     })
 
   return function unbindDatepickers() {
     unbound = true
     datepickers.forEach(d => d.destroy())
     timepickers.forEach(d => d.destroy())
+    dateMenus.forEach(d => d.destroy())
   }
 }
