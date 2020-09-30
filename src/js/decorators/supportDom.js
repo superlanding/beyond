f8c9c45ffaae787b3b02660d68e0b24414714917
kmsheng <kmsh3ng@gmail.com>
2020-09-30 12:42:17 +0800

    remove dom checking

--- src/js/decorators/supportDom.js
+++ src/js/decorators/supportDom.js
@@ -1,61 +1,17 @@
 import { isFunction } from '../utils'
 import createdComponents from '../consts/createdComponents'
 
-const domMap = new Map()
-
 export default function supportDom(target) {
-
-  const targetName = target.name
 
   return class extends target {
 
     init() {
-      if (this.classNameUsed) {
-        console.warn(`This dom has already been initialized by ${targetName}`, this.dom)
-        return
-      }
-      this.setClassNameByDom(target)
-
       this._listeners = []
       this._externalListeners = []
       if (isFunction(super.init)) {
         super.init()
       }
       createdComponents.push(this)
-    }
-
-    get classNameUsed() {
-      if (this._skipDomChecking) {
-        return false
-      }
-      const classnames = domMap.get(this.dom) || []
-      return classnames.includes(targetName)
-    }
-
-    setClassNameByDom(target) {
-      const { dom } = this
-      if (dom) {
-        const classes = domMap.get(dom) || []
-        classes.push(targetName)
-        domMap.set(dom, classes)
-      }
-    }
-
-    deleteClassNameByDom(target) {
-      const { dom } = this
-      if (! dom) {
-        return
-      }
-      const { name } = target
-      const classnames = (domMap.get(dom) || [])
-        .filter(classname => classname !== name)
-
-      if (classnames.length === 0) {
-        domMap.delete(dom)
-      }
-      else {
-        domMap.set(dom, classnames)
-      }
     }
 
     on(name, func) {
@@ -80,7 +36,6 @@ export default function supportDom(target) {
     }
 
     destroy() {
-      this.deleteClassNameByDom(target)
       this._externalListeners.length = 0
       this.removeEvents()
       if (isFunction(super.destroy)) {
