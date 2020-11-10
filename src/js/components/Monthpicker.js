1a58821c72d24ee6a4386e835f8b868fd79c6aee
kmsheng <kmsh3ng@gmail.com>
2020-11-10 17:21:39 +0800

    update Monthpicker

--- src/js/components/Monthpicker.js
+++ src/js/components/Monthpicker.js
@@ -2,18 +2,20 @@ import MonthInput from './MonthInput'
 import MonthMenu from './MonthMenu'
 import supportDom from '../decorators/supportDom'
 import { DEFAULT_TIMEZONE } from '../consts'
+import { noop, parse } from '../utils'
 
 @supportDom
 export default class Monthpicker {
 
-  constructor(dom, date, options = {}) {
+  constructor(dom, options = {}) {
     this.dom = dom
     this.tz = options.tz || DEFAULT_TIMEZONE
-    this.date = date || new Date()
+    this.date = options.date
     this.menuDate = this.date
 
     this.options = options
-    this.options.change = options.change || noop
+    this.backdropMode = options.backdropMode || 'auto'
+    this.change = options.change || noop
     this.init()
   }
 
@@ -25,12 +27,60 @@ export default class Monthpicker {
       this.options
     )
     this.monthMenu = new MonthMenu({
-      date: this.menuDate
+      date: this.menuDate,
+      change: date => {
+        this.monthInput.setDate(date)
+        this.monthInput.clearStatus()
+        this.monthMenu.hide()
+      }
     })
     this.addEvents()
   }
 
+  handleMonthInputFocus() {
+    const { monthInput } = this
+    monthInput.clearStatus()
+    monthInput.setActive(true)
+    this.monthMenu.show(this.dom)
+  }
+
+  handleMonthInputKeyUp(event) {
+    const { monthInput } = this
+    const { value } = event.target
+    const res = parse(value, monthInput.datePattern, this.date)
+
+    if (res.toString() === 'Invalid Date') {
+      monthInput.setDanger(true)
+      this.nextDate = null
+    }
+    else {
+      monthInput.setDanger(false)
+      this.nextDate = res
+    }
+  }
+
+  handleMonthInputBlur() {
+    if (this.nextDate) {
+      this.date = this.nextDate
+      this.menuDate = this.date
+      this.monthMenu.setDate(this.menuDate)
+      this.nextDate = null
+    }
+  }
+
   addEvents() {
+    const { monthInput } = this
+    monthInput.on('focus', () => this.handleMonthInputFocus())
+    monthInput.on('click', event => event.stopPropagation())
+    monthInput.on('keyup', event => this.handleMonthInputKeyUp(event))
+    monthInput.on('blur', () => this.handleMonthInputBlur())
+
+    if (this.backdropMode === 'auto') {
+      this.addEvent(document, 'click', () => {
+        this.monthMenu.hide()
+        this.monthInput.clearStatus()
+      })
+    }
   }
 
   destroy() {
