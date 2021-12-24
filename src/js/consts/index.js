f67cfd355a4e125acc65020403ceebe5603467e7
kmsheng <kmsh3ng@gmail.com>
2021-12-24 16:03:13 +0800

    add const CHART_STYLE

--- src/js/consts/index.js
+++ src/js/consts/index.js
@@ -13,6 +13,39 @@ export const THEMES = [
   { text: 'Dark', value: THEME_DARK }
 ]
 
+export const CHART_STYLE = {
+  [THEME_DEFAULT]: {
+    bg: '#fff',
+    txt: '#3c4257',
+    line: 'rgba(224, 224, 224, .5)',
+    glowAlpha: .2,
+    variants: [
+      '#5469d4',
+      '#7c54d4',
+      '#a254d4',
+      '#c040a2',
+      '#ff5604',
+      '#0be4e3',
+      '#00d924'
+    ]
+  },
+  [THEME_DARK]: {
+    bg: '#000',
+    txt: '#fff',
+    line: 'rgba(255, 255, 255, .3)',
+    glowAlpha: .3,
+    variants: [
+      '#5469d4',
+      '#7c54d4',
+      '#a254d4',
+      '#c040a2',
+      '#ff5604',
+      '#0be4e3',
+      '#00d924'
+    ]
+  }
+}
+
 export const DEFAULT_CHART_STYLES = [
   '#5469d4',
   '#7c54d4',
