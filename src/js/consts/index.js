f7a3da5372503e631c141be83c03ad3f368dcb50
kmsheng <kmsh3ng@gmail.com>
2021-12-24 16:03:02 +0800

    add const THEMES

--- src/js/consts/index.js
+++ src/js/consts/index.js
@@ -4,6 +4,15 @@ export const DEFAULT_TIMEZONE = 'Asia/Taipei'
 
 export const DEFAULT_LOCALE = locale
 
+export const THEME_DEFAULT = 'default'
+
+export const THEME_DARK = 'dark'
+
+export const THEMES = [
+  { text: 'Default', value: THEME_DEFAULT },
+  { text: 'Dark', value: THEME_DARK }
+]
+
 export const DEFAULT_CHART_STYLES = [
   '#5469d4',
   '#7c54d4',
